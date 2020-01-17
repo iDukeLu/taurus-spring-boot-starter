170ab0df14f5e49dc30c92f01ed23890db92bf61
Duke Lu <2315328598@qq.com>
2020-01-17 11:00:43 +0800

    update cache config

--- src/main/java/com/idukelu/starters/taurus/spring/boot/starter/configuration/CaffeineConfiguration.java
+++ src/main/java/com/idukelu/starters/taurus/spring/boot/starter/configuration/CaffeineConfiguration.java
@@ -1,20 +1,20 @@
 package com.idukelu.starters.taurus.spring.boot.starter.configuration;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import com.idukelu.starters.taurus.spring.boot.starter.constant.CaffeineCaches;
+import com.idukelu.starters.taurus.spring.boot.starter.constant.CacheConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCache;
-import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
+@Slf4j
 @Configuration
 @ConditionalOnClass({CaffeineCache.class, CacheManager.class})
 public class CaffeineConfiguration {
@@ -24,17 +24,35 @@ public class CaffeineConfiguration {
      * @return 缓存管理器
      */
     @Bean
-    @Primary
     public CacheManager caffeineCacheManager() {
-        SimpleCacheManager cacheManager = new SimpleCacheManager();
-        List<CaffeineCache> caffeineCaches = Arrays.stream(CaffeineCaches.values())
-                .map(c -> new CaffeineCache(c.name(),
-                        Caffeine.newBuilder().recordStats()
-                                .maximumSize(c.getMaxSize())
-                                .expireAfterAccess(c.getTtl(), TimeUnit.SECONDS)
-                                .build())).collect(Collectors.toList());
-        cacheManager.setCaches(caffeineCaches);
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager(CacheConstants.CAFFEINE);
+        cacheManager.setCaffeine(caffeineCache());
         return cacheManager;
+    }
+
+    @Bean
+    public CacheLoader<Object, Object> cacheLoader() {
+        return new CacheLoader<Object, Object>() {
+            @Override
+            public Object load(@NonNull Object key) throws Exception {
+                return null;
+            }
+
+            // 重写这个方法将oldValue值返回回去，进而刷新缓存
+            @Override
+            public Object reload(@NonNull Object key, @NonNull Object oldValue) throws Exception {
+                return oldValue;
+            }
+        };
+    }
+
+    private Caffeine<Object, Object> caffeineCache() {
+        return Caffeine.newBuilder()
+                .initialCapacity(CacheConstants.DEFAULT_INIT_SIZE)
+                .maximumSize(CacheConstants.DEFAULT_MAX_SIZE)
+                .expireAfterAccess(CacheConstants.DEFAULT_EXPIRE_TIME, TimeUnit.SECONDS)
+                .refreshAfterWrite(CacheConstants.DEFAULT_REFRESH_TIME, TimeUnit.SECONDS)
+                .recordStats();
     }
 }
 
---
+++ src/main/java/com/idukelu/starters/taurus/spring/boot/starter/constant/CacheConstants.java
@@ -0,0 +1,14 @@
+package com.idukelu.starters.taurus.spring.boot.starter.constant;
+
+public class CacheConstants {
+
+    public static final String CAFFEINE = "caffeine";
+
+    public static final int DEFAULT_INIT_SIZE = 1_000;
+
+    public static final int DEFAULT_MAX_SIZE = 10_000;
+
+    public static final int DEFAULT_EXPIRE_TIME = 10;
+
+    public static final int DEFAULT_REFRESH_TIME = 5;
+}
